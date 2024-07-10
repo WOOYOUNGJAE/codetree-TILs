aa8ff6bb6a251db148d60db6e6258ce83375d252
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2024-07-10 20:51:43 +0900

    Title: 선두를 지켜라 3, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240710/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+enum FLAG
+{
+    FLAG_NONE, FLAG_A, FLAG_B, FLAG_AB
+};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N = 0, M = 0;
+    cin >> N >> M;
+
+    int distancePerTimeA[1000]{};
+    int distancePerTimeB[1000]{};
+
+    int timeIndex = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        int v = 0, t = 0;
+        cin >> v >> t;
+
+        for (int j = 0; j < t; ++j, ++timeIndex)
+        {
+            if (timeIndex == 0)
+            {
+                distancePerTimeA[0] = v;
+            }
+            distancePerTimeA[timeIndex] = distancePerTimeA[timeIndex - 1] + v;
+        }
+    }
+    int totalTime = --timeIndex;
+
+    timeIndex = 0;
+    for (int i = 0; i < M; ++i)
+    {
+        int v = 0, t = 0;
+        cin >> v >> t;
+
+        for (int j = 0; j < t; ++j, ++timeIndex)
+        {
+            if (timeIndex == 0)
+            {
+                distancePerTimeB[0] = v;
+            }
+            distancePerTimeB[timeIndex] = distancePerTimeB[timeIndex - 1] + v;
+        }
+    }
+
+    FLAG ePrevFlag = FLAG_NONE;
+    timeIndex = 0;
+    int result = 0;
+    for (int i = 0; i < totalTime; ++i, ++timeIndex)
+    {
+        FLAG eCurFlag = FLAG_NONE;
+
+        if (distancePerTimeA[timeIndex] > distancePerTimeB[timeIndex])
+        {
+            eCurFlag = FLAG_A;
+        }
+        else if (distancePerTimeA[timeIndex] < distancePerTimeB[timeIndex])
+        {
+            eCurFlag = FLAG_B;
+        }
+        else
+        {
+            eCurFlag = FLAG_AB;
+        }
+
+        if (eCurFlag != ePrevFlag)
+        {
+            ++result;
+            ePrevFlag = eCurFlag;
+        }
+    }
+    cout << result;
+    return 0;
+}
